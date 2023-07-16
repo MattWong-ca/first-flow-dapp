54d81b8469fd02dcdbaed1f4b1efc7df8693f3aa
MattWong-ca <matthew.yt.wong@gmail.com>
2023-07-15 20:28:04 -0400

    Update minting feature + final touches

--- frontend/src/cadence/transactions/mintNFT_tx.js
+++ frontend/src/cadence/transactions/mintNFT_tx.js
@@ -1,9 +1,11 @@
 export const mintNFT = 
 `
 // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-import BottomShot from 0x990bf8fe740942e2  
-// This remains the same 
+import BirdBlock from 0x990bf8fe740942e2 
+
+// Do not change these
 import NonFungibleToken from 0x631e88ae7f1d7c20
+import MetadataViews from 0x631e88ae7f1d7c20
 
 transaction(
   recipient: Address,
@@ -12,20 +14,20 @@ transaction(
   thumbnail: String,
 ) {
   prepare(signer: AuthAccount) {
-    if signer.borrow<&BottomShot.Collection>(from: BottomShot.CollectionStoragePath) != nil {
+    if signer.borrow<&BirdBlock.Collection>(from: BirdBlock.CollectionStoragePath) != nil {
       return
     }
 
     // Create a new empty collection
-    let collection <- BottomShot.createEmptyCollection()
+    let collection <- BirdBlock.createEmptyCollection()
 
     // save it to the account
-    signer.save(<-collection, to: BottomShot.CollectionStoragePath)
+    signer.save(<-collection, to: BirdBlock.CollectionStoragePath)
 
     // create a public capability for the collection
-    signer.link<&{NonFungibleToken.CollectionPublic}>(
-      BottomShot.CollectionPublicPath,
-      target: BottomShot.CollectionStoragePath
+    signer.link<&{NonFungibleToken.CollectionPublic, MetadataViews.ResolverCollection}>(
+      BirdBlock.CollectionPublicPath,
+      target: BirdBlock.CollectionStoragePath
     )
   }
 
@@ -33,12 +35,12 @@ transaction(
   execute {
     // Borrow the recipient's public NFT collection reference
     let receiver = getAccount(recipient)
-      .getCapability(BottomShot.CollectionPublicPath)
+      .getCapability(BirdBlock.CollectionPublicPath)
       .borrow<&{NonFungibleToken.CollectionPublic}>()
       ?? panic("Could not get receiver reference to the NFT Collection")
 
     // Mint the NFT and deposit it to the recipient's collection
-    BottomShot.mintNFT(
+    BirdBlock.mintNFT(
       recipient: receiver,
       name: name,
       description: description,
